9c5355a2e698b71517db59e7a71f455a0c802f1b
Alvin Lin <alvin.lin.dev@gmail.com>
2016-01-23 09:44:41 -0500

    pushed server-client relay

--- server.js
+++ server.js
@@ -6,43 +6,47 @@
 
 var DEV_MODE = false;
 var FRAME_RATE = 1000.0 / 60.0;
-var IP = process.env.IP || 'localhost';
+var IP = process.env.IP || "localhost";
 var PORT_NUMBER = process.env.PORT || 5000;
 
 process.argv.forEach(function(value, index, array) {
-  if (value == '--dev' || value == '--development') {
+  if (value == "--dev" || value == "--development") {
     DEV_MODE = true;
   }
 });
 
 // Dependencies.
-var express = require('express');
-var http = require('http');
-var morgan = require('morgan');
-var socketIO = require('socket.io');
-var swig = require('swig');
+var express = require("express");
+var http = require("http");
+var morgan = require("morgan");
+var socketIO = require("socket.io");
+var swig = require("swig");
+
+var ClientManager = require("./ClientManager");
 
 // Initialization.
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 
-app.engine('html', swig.renderFile);
+var clientManager = new ClientManager();
 
-app.set('port', PORT_NUMBER);
-app.set('view engine', 'html');
+app.engine("html", swig.renderFile);
 
-app.use(morgan(':date[web] :method :url :req[header] :remote-addr :status'));
-app.use('/bower_components',
-        express.static(__dirname + '/bower_components'));
-app.use('/static',
-        express.static(__dirname + '/static'));
-app.use('/shared',
-        express.static(__dirname + '/shared'));
+app.set("port", PORT_NUMBER);
+app.set("view engine", "html");
+
+app.use(morgan(":date[web] :method :url :req[header] :remote-addr :status"));
+app.use("/bower_components",
+        express.static(__dirname + "/bower_components"));
+app.use("/static",
+        express.static(__dirname + "/static"));
+app.use("/shared",
+        express.static(__dirname + "/shared"));
 
 // Routing
-app.get('/', function(request, response) {
-  response.render('index.html', {
+app.get("/", function(request, response) {
+  response.render("index.html", {
     dev_mode: DEV_MODE
   });
 });
@@ -50,20 +54,33 @@ app.get('/', function(request, response) {
 // Server side input handler, modifies the state of the players and the
 // game based on the input it receives. Everything runs asynchronously with
 // the game loop.
-io.on('connection', function(socket) {
+io.on("connection", function(socket) {
   // When a new player joins, the server adds a new player to the game.
-  socket.on('new-player', function(data) {
-    socket.emit('uid',
+  socket.on("new-device", function(data) {
+    var uid = "";
+    if (data.deviceType == "phone") {
+      uid = clientManager.addClient(socket);
+    } else {
+      uid = clientManager.addComputer(socket);
+    }
+    socket.emit("uid", {
+      uid: uid
+    });
   });
 
-  // Update the internal object states every time a player sends an intent
-  // packet.
-  socket.on('player-action', function(data) {
-    io.sockets.emit('accel-data', data);
+  socket.on("link-devices", function(data) {
+    clientManager.linkClients(data.phoneDeviceUid, data.computerDeviceUid);
+  });
+
+  socket.on("phone-accel", function(data) {
+    var connectedComputerUid = clientManager.getPhone(
+      data.uid).paired_computer;
+    var computerSocket = clientManager.getComputer(connectedComputer).socket;
+    computerSocket.emit("accel-data", data);
   });
 
   // When a player disconnects, remove them from the game.
-  socket.on('disconnect', function() {
+  socket.on("disconnect", function() {
   });
 });
 
@@ -74,8 +91,8 @@ setInterval(function() {
 
 // Starts the server.
 server.listen(PORT_NUMBER, function() {
-  console.log('Starting server on port ' + PORT_NUMBER);
+  console.log("Starting server on port " + PORT_NUMBER);
   if (DEV_MODE) {
-    console.log('DEVELOPMENT MODE ENABLED');
+    console.log("DEVELOPMENT MODE ENABLED");
   }
 });
